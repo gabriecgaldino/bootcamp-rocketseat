f10ba8262ca5a4d1242b03728f0f4f600a7c0cb4
gabrielcgaldino <gabrielcgaldiino@gmail.com>
2022-03-24 23:50:17 -0300

    Concluida config webpack, iniciando modularização

---
+++ front-end/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header(){
+    return (
+        <header>
+            <h1>React JSX</h1>
+            <ul>
+                <li> Gabriel </li>
+                <li> Galdino </li>
+                <img src='.//home/gabriel/Imagens/pp.jpeg'></img>
+            </ul>
+        </header>
+    )
+}
---
+++ front-end/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react' 
+import { render } from 'react-dom'
+
+import App from './App'
+// JSX - Html dentro do JavaScript 
+render(<App/>, document.getElementById('app'))
