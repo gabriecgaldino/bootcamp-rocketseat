4531af0b243d3c95fc66870c18e97b7b7d0a8473
gabrielcgaldino <gabrielcgaldiino@gmail.com>
2022-03-25 18:43:57 -0300

    Concluida config webpack, iniciando modularização

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -4,11 +4,6 @@ export default function Header(){
     return (
         <header>
             <h1>React JSX</h1>
-            <ul>
-                <li> Gabriel </li>
-                <li> Galdino </li>
-                <img src='.//home/gabriel/Imagens/pp.jpeg'></img>
-            </ul>
         </header>
     )
 }
